6f28ef69a8c3d2b7f9c83a9a5691a2b82abf8f67
KentaYoshidome <kenta.yoshidome@ctc-g.co.jp>
2016-02-24 20:36:43 +0900

    todo-list-create

--- client/app.js
+++ client/app.js
@@ -1,8 +1,45 @@
-var myApplication = {
-  controller: function(){},
-  view:function(controller){
-    return m("h1","hello world")
+//モデルクラスTodo :２つプロパティがある
+
+var Todo = function (data) {
+  this.description = m.prop(data.description);
+  this.done = m.prop(false);
+};
+
+
+//ビューモデル
+//作成可能か判断スレするロジック
+
+var vm = {
+  init: function (){
+    vm.list = m.prop([]);
+    vm.description = m.prop("");
+    vm.add = function (){
+      if (vm.description()){
+          vm.list().push(new Todo({description: vm.description()}));
+          vm.description("");
+      }
+    };
   }
 };
 
-m.mount(document.getElementById("root"), myApplication);
+//コントローラ
+function controller(){
+  vm.init();
+}
+
+//ビュー
+function view(){
+  return m("div", [m("input", {onchange: m.withAttr("value", vm.description), value: vm.description()}),
+                   m("button", {onclick: vm.add}, "追加"),
+                   m("table", vm.list().map(function (task){
+                     return m("tr", [
+                       m("td",[
+                        m("input[type=checkbox]", {onclick:m.withAttr("checked", task.done), value: task.done()})
+                      ]),
+                      m("td", {style: {textDecoration: task.done() ? "line-through" : "none"}}, task.description())
+                    ]);
+                   }))
+                 ]);
+}
+
+m.mount(document.getElementById('root'), {controller: controller, view: view});
